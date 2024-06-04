8e6b2282e9e2eed601b7a7e839f4ce89909714a7
Sasini Wanigathunga <wanigathungasasini@gmail.com>
2024-06-05 00:25:20 +0530

    transformer

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 import torch
 
 # hyperparameters
-batch_size = 32
-block_size = 8
+batch_size = 64
+block_size = 256
 max_iters = 5000
-learning_rate = 1e-3
-eval_interval = 300
+learning_rate = 3e-4
+eval_interval = 500
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+num_layers = 6
+num_heads = 6
+dropout = 0.2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 with open('shakespere.txt', 'r', encoding = 'utf-8') as f:
--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from config import vocab_size, n_embd, block_size, device
+from config import vocab_size, n_embd, block_size, device, num_layers, num_heads, dropout
 
 
 class Head(nn.Module):
@@ -11,6 +11,8 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias = False)
         self.value = nn.Linear(n_embd, head_size, bias = False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self, x):
         B, T, C = x.shape
@@ -20,6 +22,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1)  * C**(-0.5)  # (B,T,T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B,T,T)
         wei = F.softmax(wei, dim = -1) # (B, T, T)
+        wei = self.dropout(wei)
         # perform weighted aggregation of values
         v = self.value(x)  # (B,T,head_size)
         out = wei @ v  # (B,T,T) @ (B,T,head_size) = (B,T,head_size)
@@ -29,16 +32,51 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim = -1)
+        out = torch.cat([h(x) for h in self.heads], dim = -1)
+        out = self.proj(out)
+        out = self.dropout(out)
+        return out
+    
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class TransformerBlock(nn.Module):
+    """Transformer Block with a self attention layer and a feedforward layer. (communication followed by computation)"""
+
+    def __init__(self, n_embd, num_heads):
+        super().__init__()
+        head_size = n_embd // num_heads
+        self.sa_heads = MultiHeadAttention(num_heads, head_size) # 4 heads of 32//4 = 8 dimensional self attention , then concatenate to get size 32 = n_embd
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa_heads(self.ln1(x))  # apply one head of self attention # (B,T,n_embd)
+        x = x + self.ffwd(self.ln2(x)) # (B,T,n_embd)
+        return x
 
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd) 
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(num_heads = 4, head_size = n_embd//4) # 4 heads of 32//4 = 8 dimensional self attention , then concatenate to get size 32 = n_embd
+        self.blocks = nn.Sequential(*[TransformerBlock(n_embd, num_heads=num_heads) for _ in range(num_layers)])
+        self.ln_f = nn.LayerNorm(n_embd)  # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets = None):
@@ -46,7 +84,8 @@ class BigramLanguageModel(nn.Module):
         tok_embd = self.token_embedding_table(idx)  # (B,T,n_embd)
         pos_embd = self.position_embedding_table(torch.arange(idx.shape[1], device = device))  # (T,n_embd)
         x = tok_embd + pos_embd  # (B,T,n_embd)
-        x = self.sa_heads(x)  # apply one head of self attention # (B,T,n_embd)
+        x = self.blocks(x)  # (B,T,n_embd)
+        x = self.ln_f(x)
         logits = self.lm_head(x)  # (B,T,vocab_size)
 
         if targets == None:
